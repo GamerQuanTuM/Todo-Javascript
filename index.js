2dd21564eb40137e293d5f47be1510d7881b58af
GamerQuanTuM <76802373+GamerQuanTuM@users.noreply.github.com>
2023-03-31 09:08:07 +0530

    Added Edit Functionality

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Import stylesheets
 import './style.css';
 
 let addButton = document.querySelector('.todo-btn');
@@ -6,61 +5,72 @@ let todoInput = document.querySelector('.todo-input');
 let todoList = document.querySelector('.todo-list'); //ul
 let removeAll = document.querySelector('#remove-btn');
 
-console.log(removeAll)
+let currentlyEditing = null;
 
-function AddTodos(e) {
+function addTodos(e) {
   e.preventDefault();
   let input = todoInput.value;
-  if (todoInput.value === '') {
+  if (input === '') {
     return;
   }
-  let todoItem = document.createElement('li'); //li
-  todoItem.className = 'todo-item';
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.className = 'todo-checkbox';
+  if (currentlyEditing) { 
+    currentlyEditing.querySelector('.todo-text').textContent = input;
+    todoInput.value = '';
+    currentlyEditing = null;
+    addButton.textContent = 'Add'; 
+  } else { 
+    let todoItem = document.createElement('li'); //li
+    todoItem.className = 'todo-item';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'todo-checkbox';
 
-  checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {
-      text.classList.add('checked');
-    } else {
-      text.classList.remove('checked');
+    checkbox.addEventListener('change', function () {
+      if (checkbox.checked) {
+        text.classList.add('checked');
+      } else {
+        text.classList.remove('checked');
+      }
+    });
+
+    let text = document.createElement('span');
+    text.className = 'todo-text';
+    text.style.color = 'black';
+    text.textContent = input;
+
+    let editButton = document.createElement('button');
+    editButton.className = 'delete-btn'; // Rename the class to "edit-btn"
+    editButton.textContent = 'Edit';
+    editButton.style.marginRight = '10px';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'delete-btn';
+    deleteButton.textContent = 'Delete';
+
+    todoList.appendChild(todoItem);
+    todoItem.appendChild(checkbox);
+    todoItem.appendChild(text);
+    todoItem.appendChild(editButton);
+    todoItem.appendChild(deleteButton);
+
+    function removeList() {
+      todoItem.remove();
     }
-  });
-
-  let text = document.createElement('span');
-  text.className = 'todo-text';
-  text.style.color = 'black';
-  text.textContent = input;
-
-  let editButton = document.createElement('button');
-  editButton.className = 'delete-btn';
-  editButton.textContent = 'Edit';
-  editButton.style.marginRight="10px"
-  
-
-  let deleteButton = document.createElement('button');
-  deleteButton.className = 'delete-btn';
-  deleteButton.textContent = 'Delete';
-  
-
-  todoList.appendChild(todoItem);
-  todoItem.appendChild(checkbox);
-  todoItem.appendChild(text);
-  todoItem.appendChild(editButton);
-  todoItem.appendChild(deleteButton);
-
-  function removeList() {
-    todoItem.remove();
+    function editList() {
+      todoInput.value = text.textContent;
+      currentlyEditing = todoItem; 
+      addButton.textContent = 'Save';
+    }
+    deleteButton.addEventListener('click', removeList);
+    editButton.addEventListener('click', editList);
+    todoInput.value = '';
   }
-  deleteButton.addEventListener('click', removeList);
-  todoInput.value = ''; 
 }
 
-removeAll.addEventListener("click",function(){
+removeAll.addEventListener('click', function () {
   while (todoList.firstChild) {
     todoList.removeChild(todoList.firstChild);
   }
 });
 
-addButton.addEventListener('click', AddTodos);
+addButton.addEventListener('click', addTodos);
